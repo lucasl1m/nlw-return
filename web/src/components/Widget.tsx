7823ea0928a786f98c7ede082899ab4eaa6e9220
Lucas Araújo de Lima <lucas.araujo.lima@ccc.ufcg.edu.br>
2022-05-05 23:47:27 -0300

    Created feedback components and functionality

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import { ChatTeardropDots } from "phosphor-react";
 
-import { Popover } from "@headlessui/react"
+import { Popover } from "@headlessui/react";
+import { WidgetForm } from "./WidgetForm";
 
 export const Widget: React.FC = () => {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false);
@@ -12,11 +13,15 @@ export const Widget: React.FC = () => {
   }
 
   return (
-    <Popover className="absolute bottom-5 right-5">
+    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
 
-      <Popover.Panel>Hello World!</Popover.Panel>
-
-      <Popover.Button className="bg-violet-500 rounded-full px-3 h-12 flex items-center group" onClick={toogleWidgetVisibility}>
+      <Popover.Button
+        className="bg-brand-500 rounded-full px-3 h-12 flex items-center group"
+        onClick={toogleWidgetVisibility}
+      >
         <ChatTeardropDots className="w-6 h-6 text-white" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
